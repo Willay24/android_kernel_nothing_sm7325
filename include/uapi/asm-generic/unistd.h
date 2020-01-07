776adc4661c210b04b7c91074beb22b7213a8d5a
Sargun Dhillon <sargun@sargun.me>
2020-01-07 09:59:26 -0800

    arch: wire up pidfd_getfd syscall
    This wires up the pidfd_getfd syscall for all architectures.
    
    Signed-off-by: Sargun Dhillon <sargun@sargun.me>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20200107175927.4558-4-sargun@sargun.me
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    (cherry picked from commit 9a2cef09c801de54feecd912303ace5c27237f12)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -853,6 +853,8 @@ __SYSCALL(__NR_clone3, sys_clone3)
 #define __NR_close_range 436
 __SYSCALL(__NR_close_range, sys_close_range)
 #define __NR_openat2 437
+#define __NR_pidfd_getfd 438
+__SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
 #define __NR_faccessat2 439
 __SYSCALL(__NR_faccessat2, sys_faccessat2)
 #define __NR_process_madvise 440
