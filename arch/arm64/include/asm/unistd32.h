1b6d4b36423b440036d3d78c9b1d3fb8119149ef
Aleksa Sarai <cyphar@cyphar.com>
2020-01-18 23:07:59 +1100

    BACKPORT: open: introduce openat2(2) syscall
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -881,6 +881,7 @@ __SYSCALL(__NR_pidfd_open, sys_pidfd_open)
 __SYSCALL(__NR_clone3, sys_clone3)
 #define __NR_close_range 436
 __SYSCALL(__NR_close_range, sys_close_range)
+#define __NR_openat2 437
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
 #define __NR_epoll_pwait2 441
