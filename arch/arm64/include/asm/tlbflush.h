4d678cf86c7ad5b862534b4d36367ef76e8e956e
Zhenyu Ye <yezhenyu2@huawei.com>
2020-06-25 16:03:13 +0800

    arm64: tlb: Set the TTL field in flush_tlb_range
    This patch uses the cleared_* in struct mmu_gather to set the
    TTL field in flush_tlb_range().
    
    Signed-off-by: Zhenyu Ye <yezhenyu2@huawei.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20200625080314.230-6-yezhenyu2@huawei.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Change-Id: I048e8933aeb6983e51b96bd7ff05c220b9e5c1f5
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -264,7 +264,8 @@ static inline void arch_tlbbatch_flush(struct arch_tlbflush_unmap_batch *batch)
 
 static inline void __flush_tlb_range(struct vm_area_struct *vma,
 				     unsigned long start, unsigned long end,
-				     unsigned long stride, bool last_level)
+				     unsigned long stride, bool last_level,
+				     int tlb_level)
 {
 	unsigned long asid, addr;
 
@@ -286,11 +287,11 @@ static inline void __flush_tlb_range(struct vm_area_struct *vma,
 
 	for (addr = start; addr < end; addr += stride) {
 		if (last_level) {
-			__tlbi(vale1is, addr);
-			__tlbi_user(vale1is, addr);
+			__tlbi(vale1is, addr, tlb_level);
+			__tlbi_user(vale1is, addr, tlb_level);
 		} else {
-			__tlbi(vae1is, addr);
-			__tlbi_user(vae1is, addr);
+			__tlbi(vae1is,  addr, tlb_level);
+			__tlbi_user(vae1is, addr, tlb_level);
 		}
 	}
 	dsb(ish);
@@ -302,8 +303,9 @@ static inline void flush_tlb_range(struct vm_area_struct *vma,
 	/*
 	 * We cannot use leaf-only invalidation here, since we may be invalidating
 	 * table entries as part of collapsing hugepages or moving page tables.
+	 * Set the tlb_level to 0 because we can not get enough information here.
 	 */
-	__flush_tlb_range(vma, start, end, PAGE_SIZE, false);
+	__flush_tlb_range(vma, start, end, PAGE_SIZE, false, 0);
 }
 
 static inline void flush_tlb_kernel_range(unsigned long start, unsigned long end)
