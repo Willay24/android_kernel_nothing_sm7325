4bdadecafcea3105cdfa898d50a9f8a8fc7cd222
Colin Ian King <colin.king@canonical.com>
2020-09-10 16:12:21 +0100

    binder: remove redundant assignment to pointer n
    The pointer n is being initialized with a value that is
    never read and it is being updated later with a new value. The
    initialization is redundant and can be removed.
    
    Acked-by: Todd Kjos <tkjos@google.com>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Link: https://lore.kernel.org/r/20200910151221.751464-1-colin.king@canonical.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -425,7 +425,7 @@ static bool debug_low_async_space_locked(struct binder_alloc *alloc)
 	 * and at some point we'll catch them in the act. This is more efficient
 	 * than keeping a map per pid.
 	 */
-	struct rb_node *n = alloc->free_buffers.rb_node;
+	struct rb_node *n;
 	struct binder_buffer *buffer;
 	size_t total_alloc_size = 0;
 	int pid = current->tgid;
