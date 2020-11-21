2048aba5037868bf28c884d2bc082197db00ad36
Saravana Kannan <saravanak@google.com>
2020-11-20 18:02:32 -0800

    UPSTREAM: driver core: Delete pointless parameter in fwnode_operations.add_links
    The struct device input to add_links() is not used for anything. So
    delete it.
    
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20201121020232.908850-18-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2d09e6eb4a6f20273959f4905ccf009da8c64c7a)
    Bug: 178143855
    Bug: 169667932
    Change-Id: I9def5787f37ab27f9640f3fcbff4bf34abf6c7e2
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1477,7 +1477,7 @@ static void fw_devlink_parse_fwnode(struct fwnode_handle *fwnode)
 	if (fwnode->flags & FWNODE_FLAG_LINKS_ADDED)
 		return;
 
-	fwnode_call_int_op(fwnode, add_links, NULL);
+	fwnode_call_int_op(fwnode, add_links);
 	fwnode->flags |= FWNODE_FLAG_LINKS_ADDED;
 }
 
