ac2520950bbe718e5dfbd2a2e949bbf1bea871df
Gao Xiang <hsiangkao@redhat.com>
2020-12-08 17:58:32 +0800

    UPSTREAM: erofs: get rid of magical Z_EROFS_MAPPING_STAGING
    Previously, we played around with magical page->mapping for short-lived
    temporary pages since we need to identify different types of pages in
    the same pcluster but both invalidated and short-lived temporary pages
    can have page->mapping == NULL. It was considered as safe because that
    temporary pages are all non-LRU / non-movable pages.
    
    This patch tends to use specific page->private to identify short-lived
    pages instead so it won't rely on page->mapping anymore. Details are
    described in "compress.h" as well.
    
    Link: https://lore.kernel.org/r/20201208095834.3133565-1-hsiangkao@redhat.com
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    
    Bug: 190585249
    Change-Id: I0beb89854846dc5fe9bf167bc9ce311659003dc3
    (cherry picked from commit 6aaa7b0664e6886f6154070edbc24435d6e1f86b)
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -754,12 +754,12 @@ hitted:
 retry:
 	err = z_erofs_attach_page(clt, page, page_type,
 				  clt->mode >= COLLECT_PRIMARY_FOLLOWED);
-	/* should allocate an additional staging page for pagevec */
+	/* should allocate an additional short-lived page for pagevec */
 	if (err == -EAGAIN) {
 		struct page *const newpage =
 				alloc_page(GFP_NOFS | __GFP_NOFAIL);
 
-		newpage->mapping = Z_EROFS_MAPPING_STAGING;
+		set_page_private(newpage, Z_EROFS_SHORTLIVED_PAGE);
 		err = z_erofs_attach_page(clt, newpage,
 					  Z_EROFS_PAGE_TYPE_EXCLUSIVE, true);
 		if (!err)
@@ -823,6 +823,11 @@ static void z_erofs_decompress_kickoff(struct z_erofs_decompressqueue *io,
 	z_erofs_decompressqueue_work(&io->u.work);
 }
 
+static bool z_erofs_page_is_invalidated(struct page *page)
+{
+	return !page->mapping && !z_erofs_is_shortlived_page(page);
+}
+
 static void z_erofs_decompressqueue_endio(struct bio *bio)
 {
 	tagptr1_t t = tagptr_init(tagptr1_t, bio->bi_private);
@@ -835,7 +840,7 @@ static void z_erofs_decompressqueue_endio(struct bio *bio)
 		struct page *page = bvec->bv_page;
 
 		DBG_BUGON(PageUptodate(page));
-		DBG_BUGON(!page->mapping);
+		DBG_BUGON(z_erofs_page_is_invalidated(page));
 
 		if (err)
 			SetPageError(page);
@@ -1134,7 +1139,7 @@ repeat:
 	mapping = READ_ONCE(page->mapping);
 
 	/*
-	 * unmanaged (file) pages are all locked solidly,
+	 * file-backed online pages in plcuster are all locked steady,
 	 * therefore it is impossible for `mapping' to be NULL.
 	 */
 	if (mapping && mapping != mc)
@@ -1189,8 +1194,8 @@ repeat:
 out_allocpage:
 	page = erofs_allocpage(pagepool, gfp | __GFP_NOFAIL);
 	if (!tocache || add_to_page_cache_lru(page, mc, index + nr, gfp)) {
-		/* non-LRU / non-movable temporary page is needed */
-		page->mapping = Z_EROFS_MAPPING_STAGING;
+		/* turn into temporary page if fails */
+		set_page_private(page, Z_EROFS_SHORTLIVED_PAGE);
 		tocache = false;
 	}
 	attach_page_private(page, pcl);
@@ -1210,8 +1215,9 @@ out_allocpage:
 	}
 
 	if (tocache) {
-		set_page_private(page, (unsigned long)pcl);
-		SetPagePrivate(page);
+		attach_page_private(page, pcl);
+		/* drop a ref added by allocpage (then we have 2 refs here) */
+		put_page(page);
 	}
 out:	/* the only exit (for tracing and debugging) */
 	return page;
