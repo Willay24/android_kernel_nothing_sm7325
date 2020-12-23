0160fcd40a7ac1ecc827382cb4fe6487ac7742b0
Park Bumgyu <bumgyu.park@samsung.com>
2020-12-23 09:44:37 +0900

    ANDROID: vendor_hook: modify cpuidle vendor hook
    When entering cluster-wide or system-wide power mode, Exynos cpu
    power management driver checks the next hrtimer events of cpu
    composing the power domain to prevent unnecessary attempts to enter
    the power mode. Since struct cpuidle_device has next_hrtimer, it
    can be solved by passing cpuidle device as a parameter of vh.
    
    In order to improve responsiveness, it is necessary to prevent
    entering the deep idle state in boosting scenario. So, vendor
    driver should be able to control the idle state.
    
    Due to above requirements, the parameters required for idle enter
    and exit different, so the vendor hook is separated into
    cpu_idle_enter and cpu_idle_exit.
    
    Bug: 176198732
    
    Change-Id: I2262ba1bae5e6622a8e76bc1d5d16fb27af0bb8a
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -227,7 +227,7 @@ int __nocfi cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_drive
 	/* Take note of the planned idle state. */
 	sched_idle_set_state(target_state);
 
-	trace_android_vh_cpu_idle(0, index, dev->cpu);
+	trace_android_vh_cpu_idle_enter(&index, dev);
 	trace_cpu_idle(index, dev->cpu);
 	fie_idle_enter();
 	time_start = ns_to_ktime(local_clock());
@@ -241,7 +241,7 @@ int __nocfi cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_drive
 	sched_clock_idle_wakeup_event();
 	time_end = ns_to_ktime(local_clock());
 	trace_cpu_idle(PWR_EVENT_EXIT, dev->cpu);
-	trace_android_vh_cpu_idle(PWR_EVENT_EXIT, entered_state, dev->cpu);
+	trace_android_vh_cpu_idle_exit(entered_state, dev);
 	fie_idle_exit();
 
 	/* The cpu is no longer idle or about to enter idle. */
