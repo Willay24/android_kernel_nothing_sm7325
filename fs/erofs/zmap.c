caac95237f4905629feabf4f7c2d4566fe6ee34e
Gao Xiang <hsiangkao@redhat.com>
2021-04-07 12:39:18 +0800

    UPSTREAM: erofs: reserve physical_clusterbits[]
    Formal big pcluster design is actually more powerful / flexable than
    the previous thought whose pclustersize was fixed as power-of-2 blocks,
    which was obviously inefficient and space-wasting. Instead, pclustersize
    can now be set independently for each pcluster, so various pcluster
    sizes can also be used together in one file if mkfs wants (for example,
    according to data type and/or compression ratio).
    
    Let's get rid of previous physical_clusterbits[] setting (also notice
    that corresponding on-disk fields are still 0 for now). Therefore,
    head1/2 can be used for at most 2 different algorithms in one file and
    again pclustersize is now independent of these.
    
    Link: https://lore.kernel.org/r/20210407043927.10623-2-xiang@kernel.org
    Acked-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    
    Bug: 201372112
    Change-Id: I7db236f06b202949b882a366b347f0c4e9dc0c3e
    (cherry picked from commit 54e0b6c873dcbd02b9b479c893f6fba8fcbc6a9c)
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/erofs/zmap.c
+++ fs/erofs/zmap.c
@@ -21,7 +21,6 @@ int z_erofs_fill_inode(struct inode *inode)
 		vi->z_logical_clusterbits = LOG_BLOCK_SIZE;
 		set_bit(EROFS_I_Z_INITED_BIT, &vi->flags);
 	}
-
 	inode->i_mapping->a_ops = &z_erofs_aops;
 	return 0;
 }
@@ -78,22 +77,6 @@ static int z_erofs_fill_inode_lazy(struct inode *inode)
 	}
 
 	vi->z_logical_clusterbits = LOG_BLOCK_SIZE + (h->h_clusterbits & 7);
-	if (!erofs_sb_has_big_pcluster(EROFS_SB(sb)) &&
-	    vi->z_advise & (Z_EROFS_ADVISE_BIG_PCLUSTER_1 |
-			    Z_EROFS_ADVISE_BIG_PCLUSTER_2)) {
-		erofs_err(sb, "per-inode big pcluster without sb feature for nid %llu",
-			  vi->nid);
-		err = -EFSCORRUPTED;
-		goto unmap_done;
-	}
-	if (vi->datalayout == EROFS_INODE_FLAT_COMPRESSION &&
-	    !(vi->z_advise & Z_EROFS_ADVISE_BIG_PCLUSTER_1) ^
-	    !(vi->z_advise & Z_EROFS_ADVISE_BIG_PCLUSTER_2)) {
-		erofs_err(sb, "big pcluster head1/2 of compact indexes should be consistent for nid %llu",
-			  vi->nid);
-		err = -EFSCORRUPTED;
-		goto unmap_done;
-	}
 	/* paired with smp_mb() at the beginning of the function */
 	smp_mb();
 	set_bit(EROFS_I_Z_INITED_BIT, &vi->flags);
