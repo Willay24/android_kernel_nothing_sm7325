5adc8767b2bbc73aecfaeb141d50bd8b8ad5fd54
Anil Veshala <aveshala@codeaurora.org>
2021-10-12 12:01:14 +0530

    platform: msm: qcom-geni-se: DDR vote issue
    SS raised a issue sometimes DDR vote is present even we are removing
    the vote, there is a possibility of race condition where 2 driver can
    call geni_resource_init which is leading to stale BW vote as of_icc_get
    is called twice for the same QUP wrapper node, to solve this  mutex
    locking added in geni_se_resources_init.
    
    Change-Id: Idc10de1f6d832775916ef026730720ebcc266b5f
    Signed-off-by: Anil Veshala <aveshala@codeaurora.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/platform/msm/msm-geni-se.c
+++ drivers/platform/msm/msm-geni-se.c
@@ -1057,6 +1057,7 @@ int geni_se_resources_init(struct se_geni_rsc *rsc,
 	if (geni_se_dev->vectors == NULL)
 		return 0;
 
+	mutex_lock(&geni_se_dev->geni_dev_lock);
 	if (IS_ERR_OR_NULL(geni_se_dev->bus_bw)) {
 		geni_se_dev->bus_bw = icc_get(geni_se_dev->dev,
 					geni_se_dev->vectors[0].src,
@@ -1065,7 +1066,7 @@ int geni_se_resources_init(struct se_geni_rsc *rsc,
 			GENI_SE_ERR(geni_se_dev->log_ctx, false, NULL,
 				"%s: Error Get Path: (Core2x), %ld\n",
 				__func__, PTR_ERR(geni_se_dev->bus_bw));
-
+				mutex_unlock(&geni_se_dev->geni_dev_lock);
 				return geni_se_dev->bus_bw ?
 				PTR_ERR(geni_se_dev->bus_bw) : -ENOENT;
 		}
@@ -1085,7 +1086,7 @@ int geni_se_resources_init(struct se_geni_rsc *rsc,
 					PTR_ERR(geni_se_dev->bus_bw_noc));
 				icc_put(geni_se_dev->bus_bw);
 				geni_se_dev->bus_bw = NULL;
-
+				mutex_unlock(&geni_se_dev->geni_dev_lock);
 				return geni_se_dev->bus_bw_noc ?
 				PTR_ERR(geni_se_dev->bus_bw_noc) : -ENOENT;
 			}
@@ -1102,6 +1103,7 @@ int geni_se_resources_init(struct se_geni_rsc *rsc,
 
 	INIT_LIST_HEAD(&rsc->ab_list);
 	INIT_LIST_HEAD(&rsc->ib_list);
+	mutex_unlock(&geni_se_dev->geni_dev_lock);
 
 	return 0;
 }
