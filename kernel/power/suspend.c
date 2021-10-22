fb44c9c5ae857e4795f32b4d821e465b56bcfc30
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-10-22 18:07:47 +0200

    PM: sleep: Pause cpuidle later and resume it earlier during system transitions
    Commit 8651f97bd951 ("PM / cpuidle: System resume hang fix with
    cpuidle") that introduced cpuidle pausing during system suspend
    did that to work around a platform firmware issue causing systems
    to hang during resume if CPUs were allowed to enter idle states
    in the system suspend and resume code paths.
    
    However, pausing cpuidle before the last phase of suspending
    devices is the source of an otherwise arbitrary difference between
    the suspend-to-idle path and other system suspend variants, so it is
    cleaner to do that later, before taking secondary CPUs offline (it
    is still safer to take secondary CPUs offline with cpuidle paused,
    though).
    
    Modify the code accordingly, but in order to avoid code duplication,
    introduce new wrapper functions, pm_sleep_disable_secondary_cpus()
    and pm_sleep_enable_secondary_cpus(), to combine cpuidle_pause()
    and cpuidle_resume(), respectively, with the handling of secondary
    CPUs during system-wide transitions to sleep states.
    
    Change-Id: I4c034abbdbfa205c086cd0761d89c09432d581ac
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -102,6 +102,7 @@ static void s2idle_enter(void)
 	raw_spin_unlock_irq(&s2idle_lock);
 
 	get_online_cpus();
+	cpuidle_resume();
 
 	/* Push all the CPUs into the idle loop. */
 	wake_up_all_idle_cpus();
@@ -109,6 +110,7 @@ static void s2idle_enter(void)
 	swait_event_exclusive(s2idle_wait_head,
 		    s2idle_state == S2IDLE_STATE_WAKE);
 
+	cpuidle_pause();
 	put_online_cpus();
 
 	raw_spin_lock_irq(&s2idle_lock);
@@ -407,9 +409,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	if (error)
 		goto Devices_early_resume;
 
-	if (state != PM_SUSPEND_TO_IDLE)
-		cpuidle_pause();
-
 	error = dpm_suspend_noirq(PMSG_SUSPEND);
 	if (error) {
 		pr_err("noirq suspend of devices failed\n");
@@ -427,7 +426,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_wake;
 	}
 
-	error = suspend_disable_secondary_cpus();
+	error = pm_sleep_disable_secondary_cpus();
 	if (error || suspend_test(TEST_CPUS))
 		goto Enable_cpus;
 
@@ -457,16 +456,13 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	BUG_ON(irqs_disabled());
 
  Enable_cpus:
-	suspend_enable_secondary_cpus();
+	pm_sleep_enable_secondary_cpus();
 
  Platform_wake:
 	platform_resume_noirq(state);
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume:
-	if (state != PM_SUSPEND_TO_IDLE)
-		cpuidle_resume();
-
 	platform_resume_early(state);
 
  Devices_early_resume:
