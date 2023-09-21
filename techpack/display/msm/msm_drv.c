e3f18941574dbec896c44bdb55e65d8b0f834171
Juhyung Park <qkrwngud825@gmail.com>
2023-09-21 20:36:57 +0900

    drm/msm/sde: use a single module_init()
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -2197,12 +2197,15 @@ static struct platform_driver msm_platform_driver = {
 	},
 };
 
+extern int __init sde_kmem_pool_init(void);
+
 static int __init msm_drm_register(void)
 {
 	if (!modeset)
 		return -EINVAL;
 
 	DBG("init");
+	sde_kmem_pool_init();
 	sde_rsc_rpmh_register();
 	sde_rsc_register();
 	dsi_display_register();
