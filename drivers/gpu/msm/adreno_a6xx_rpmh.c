5c15f4044df33d2aa76e847b715668a8ea2cf685
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-01-12 23:07:07 +0600

    adreno_a6xx_rpmh: Fix division by zero in tcs_cmd_data()
    UBSAN warns of a division by zero for (bcms[i].buswidth * bcms[i].channels)
    in tcs_cmd_data():
    
    [    0.883871] ================================================================================
    [    0.883878] UBSAN: Undefined behaviour in ../drivers/gpu/msm/adreno_a6xx_rpmh.c:149:3
    [    0.883880] division by zero
    [    0.883883] CPU: 5 PID: 7 Comm: kworker/u16:0 Not tainted 5.4.278-Scarlet-v1.0-beta8 #1
    [    0.883884] Hardware name: redwood based Qualcomm Technologies, Inc. SM7325 (DT)
    [    0.883895] Workqueue: events_unbound deferred_probe_work_func
    [    0.883897] Call trace:
    [    0.883902] dump_backtrace+0x0/0x230
    [    0.883904] show_stack+0x14/0x20
    [    0.883909] dump_stack+0x8c/0xcc
    [    0.883913] __ubsan_handle_divrem_overflow+0x1a4/0x1d0
    [    0.883915] build_rpmh_bw_votes+0x32c/0x4f0
    [    0.883916] a6xx_build_rpmh_tables+0x4b8/0xbd0
    [    0.883918] a6xx_gmu_probe+0x284/0x400
    [    0.883920] a6xx_gmu_bind+0x20/0x90
    [    0.883921] component_bind_all+0x180/0x2f0
    [    0.883924] adreno_device_probe+0x7f4/0xab0
    [    0.883925] a6xx_probe_common+0x1f0/0x200
    [    0.883926] a6xx_gmu_device_probe+0x48/0xd0
    [    0.883927] adreno_bind+0x234/0x2f0
    [    0.883928] try_to_bring_up_master+0x150/0x200
    [    0.883929] __component_add+0xbc/0x1c0
    [    0.883930] component_add+0x10/0x20
    [    0.883931] a6xx_gmu_probe_dev+0x18/0x20
    [    0.883933] platform_drv_probe+0x70/0xf0
    [    0.883934] really_probe+0x1bc/0x430
    [    0.883935] driver_probe_device+0x68/0x160
    [    0.883936] __device_attach_driver+0x15c/0x1b0
    [    0.883937] bus_for_each_drv+0x84/0xd0
    [    0.883937] __device_attach+0xe0/0x170
    [    0.883938] device_initial_probe+0x10/0x20
    [    0.883939] bus_probe_device+0x30/0xa0
    [    0.883940] deferred_probe_work_func+0x4c/0xb0
    [    0.883943] process_one_work+0x1c4/0x310
    [    0.883944] process_scheduled_works+0x2c/0x50
    [    0.883945] worker_thread+0x35c/0x600
    [    0.883946] kthread+0x174/0x1b0
    [    0.883948] ret_from_fork+0x10/0x18
    [    0.883949] ================================================================================
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx_rpmh.c
+++ drivers/gpu/msm/adreno_a6xx_rpmh.c
@@ -125,7 +125,7 @@ static void tcs_cmd_data(struct bcm *bcms, int count, u32 ab, u32 ib,
 	for (i = 0; i < count; i++) {
 		bool valid = true;
 		bool commit = false;
-		u64 avg, peak, x, y;
+		u64 avg, total_width, peak, x, y;
 
 		if (i == count - 1 || bcms[i].vcd != bcms[i + 1].vcd)
 			commit = true;
@@ -149,8 +149,9 @@ static void tcs_cmd_data(struct bcm *bcms, int count, u32 ab, u32 ib,
 		total_width = bcms[i].buswidth * bcms[i].channels;
 
 		/* And then divide by the total width across channels */
+		total_width = bcms[i].buswidth * bcms[i].channels;
 		if (total_width)
-			do_div(avg, (u32) total_width);
+			do_div(avg, total_width);
 
 		peak = ((u64) ib) * bcms[i].width;
 		do_div(peak, bcms[i].buswidth);
