c7dc77e351cabb2380d86c56606191c789c6f23a
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-03-05 20:52:36 +0600

    techpack: camera: Compile out all debug logs on !DEBUG_KERNEL
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- techpack/camera/drivers/cam_smmu/cam_smmu_api.c
+++ techpack/camera/drivers/cam_smmu/cam_smmu_api.c
@@ -4231,7 +4231,9 @@ static int cam_smmu_fw_dev_component_bind(struct device *dev,
 static void cam_smmu_fw_dev_component_unbind(struct device *dev,
 	struct device *master_dev, void *data)
 {
+#ifdef CONFIG_DEBUG_KERNEL
 	struct platform_device *pdev = to_platform_device(dev);
+#endif
 
 	CAM_DBG(CAM_SMMU, "Unbinding component: %s", pdev->name);
 }
@@ -4261,7 +4263,9 @@ static int cam_smmu_cb_component_bind(struct device *dev,
 static void cam_smmu_cb_component_unbind(struct device *dev,
 	struct device *master_dev, void *data)
 {
+#ifdef CONFIG_DEBUG_KERNEL
 	struct platform_device *pdev = to_platform_device(dev);
+#endif
 
 	CAM_DBG(CAM_SMMU, "Unbinding component: %s", pdev->name);
 }
@@ -4289,7 +4293,9 @@ static int cam_smmu_cb_qsmmu_component_bind(struct device *dev,
 static void cam_smmu_cb_qsmmu_component_unbind(struct device *dev,
 	struct device *master_dev, void *data)
 {
+#ifdef CONFIG_DEBUG_KERNEL
 	struct platform_device *pdev = to_platform_device(dev);
+#endif
 
 	CAM_DBG(CAM_SMMU, "Unbinding component: %s", pdev->name);
 }
