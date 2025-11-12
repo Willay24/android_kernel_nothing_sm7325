196277c5d9e4ab897ffe99c1fdd00a67552ebfd8
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-11-12 17:23:24 +0100

    UPSTREAM: cpuidle: governors: teo: Drop redundant function parameter
    The last no_poll parameter of teo_find_shallower_state() is always
    false, so drop it.
    
    No intentional functional impact.
    
    Change-Id: Ie9faf7b8b5e9d7306d7ac183cbd450a9a5a591f5
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Tested-by: Christian Loehle <christian.loehle@arm.com>
    Link: https://patch.msgid.link/2253109.irdbgypaU6@rafael.j.wysocki
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -253,17 +253,15 @@ static bool teo_state_ok(int i, struct cpuidle_driver *drv)
  * @dev: Target CPU.
  * @state_idx: Index of the capping idle state.
  * @duration_us: Idle duration value to match.
- * @no_poll: Don't consider polling states.
  */
 static int teo_find_shallower_state(struct cpuidle_driver *drv,
 				    struct cpuidle_device *dev, int state_idx,
-				    int duration_us, bool no_poll)
+				     s64 duration_ns)
 {
 	int i;
 
 	for (i = state_idx - 1; i >= 0; i--) {
-		if (dev->states_usage[i].disable ||
-				(no_poll && drv->states[i].flags & CPUIDLE_FLAG_POLLING))
+		if (dev->states_usage[i].disable)
 			continue;
 
 		state_idx = i;
@@ -473,7 +471,7 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 	 * candidate state, a shallower one needs to be found.
 	 */
 	if (drv->states[idx].target_residency_ns > duration_ns)
-		idx = teo_find_shallower_state(drv, dev, idx, duration_ns, false);
+		idx = teo_find_shallower_state(drv, dev, idx, duration_ns);
 
 	/*
 	 * If the selected state's target residency is below the tick length
