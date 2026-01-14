7d671dba915b4a5b1c7323b553bd23ea25608111
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-01-14 20:45:30 +0100

    UPSTREAM: cpuidle: governors: teo: Refine tick_intercepts vs total events check
    Use 2/3 as the proportion coefficient in the check comparing
    cpu_data->tick_intercepts with cpu_data->total because it is close
    enough to the current one (5/8) and it allows of more straightforward
    interpretation (on average, intercepts within the tick period length
    are twice as frequent as other events).
    
    Change-Id: I1048c6b3e61f5184da7b472bccc4efa63c99114b
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Christian Loehle <christian.loehle@arm.com>
    Link: https://patch.msgid.link/10793374.nUPlyArG6x@rafael.j.wysocki
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -499,7 +499,7 @@ constraint:
 	 * total wakeup events, do not stop the tick.
 	 */
 	if (drv->states[idx].target_residency < TICK_USEC &&
-	    cpu_data->tick_intercepts > cpu_data->total / 2 + cpu_data->total / 8)
+	     3 * cpu_data->tick_intercepts >= 2 * cpu_data->total)
 		duration_us = TICK_USEC / 2;
 
 end:
