f924b60ad68594f59daca390101493ae699ac966
ssakore <ssakore@qti.qualcomm.com>
2026-03-03 11:52:37 +0530

    msm: adsprpc: Validate page range with map range before passing to DSP
    In case of unaligned start address and NOVA flag set, range validation
    is by-passed. The page size calculated using unaligned address passed by
    user is aligned to nearest boundary internally which returns page size
    4KB more. In order to avoid out of bounds issue, check page range is within
    map range.
    
    Acked-by: Om Deore <odeore@qti.qualcomm.com>
    Change-Id: Ie2de944206eee0730c8ba081f05b7d7cccc72276
    Signed-off-by: ssakore <ssakore@qti.qualcomm.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 /* Uncomment this block to log an error on every VERIFY failure */
@@ -2654,6 +2654,22 @@ static int get_args(uint32_t kernel, struct smq_invoke_ctx *ctx)
 			}
 			pages[idx].addr = map->phys + offset;
 			pages[idx].size = num << PAGE_SHIFT;
+			/*
+			 * Check for page range overflow and validate page
+			 * range is not greater than map buffer range.
+			 * This prevents potential buffer overflow
+			 * and memory corruption that could be exploited.
+			 */
+			if (pages[idx].addr > (ULLONG_MAX - pages[idx].size) ||
+			   (pages[idx].addr + pages[idx].size) >
+					(map->phys + map->size)) {
+				err = -EFAULT;
+				ADSPRPC_ERR(
+					"bad addr 0x%llx len %zu IPA 0x%llx size %zu fd %d\n",
+					(uintptr_t)lpra[i].buf.pv, len,
+					map->phys, map->size, map->fd);
+				goto bail;
+			}
 		}
 		rpra[i].buf.pv = buf;
 	}
