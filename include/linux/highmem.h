de81e7e24adea524e7dfadceb0e86aa046d67959
William24hmar <williamhmar24@gmail.com>
2026-06-26 21:41:18 +0000

    highmem: remove duplicate declaration

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -333,34 +333,15 @@ static inline void memcpy_to_page(struct page *page, size_t offset,
 	char *to = kmap_local_page(page);
 
 	memcpy(to + offset, from, len);
-	flush_dcache_page(page);
 	kunmap_local(to);
 }
 
 static inline void memzero_page(struct page *page, size_t offset, size_t len)
 {
 	char *addr = kmap_local_page(page);
+
 	memset(addr + offset, 0, len);
-	flush_dcache_page(page);
 	kunmap_local(addr);
 }
-
-static inline void *kmap_local_page(struct page *page)
-{
-	return page_address(page);
-}
-
-static inline void __kunmap_local(void *addr)
-{
-#ifdef ARCH_HAS_FLUSH_ON_KUNMAP
-	kunmap_flush_on_unmap(PTR_ALIGN_DOWN(addr, PAGE_SIZE));
-#endif
-}
-
-#define kunmap_local(__addr)					\
-do {								\
-	BUILD_BUG_ON(__same_type((__addr), struct page *));	\
-	__kunmap_local(__addr);					\
-} while (0)
 
 #endif /* _LINUX_HIGHMEM_H */
