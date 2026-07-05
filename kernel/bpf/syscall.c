e173d71d9feac32cf06b22a97b1fda0cedb2eeb4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:42:58 +0600

    Revert "UPSTREAM: bpf: Deref map in BPF_PROG_BIND_MAP when it's already used"
    This reverts commit 4af3414bc470c9d694269843804a10d53f677a19.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -4321,10 +4321,8 @@ static int bpf_prog_bind_map(union bpf_attr *attr)
 	used_maps_old = prog->aux->used_maps;
 
 	for (i = 0; i < prog->aux->used_map_cnt; i++)
-		if (used_maps_old[i] == map) {
-			bpf_map_put(map);
+		if (used_maps_old[i] == map)
 			goto out_unlock;
-		}
 
 	used_maps_new = kmalloc_array(prog->aux->used_map_cnt + 1,
 				      sizeof(used_maps_new[0]),
