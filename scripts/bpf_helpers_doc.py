9ca3f6117b2e7d68a71d0552ca726292575a700e
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:43:14 +0600

    Revert "UPSTREAM: bpf: Add bpf_skc_to_udp6_sock() helper"
    This reverts commit a5669bfd40de3f9a260e5212f6d735b1e9cb1692.

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -426,7 +426,6 @@ class PrinterHelpers(Printer):
             'struct tcp_sock',
             'struct tcp_timewait_sock',
             'struct tcp_request_sock',
-            'struct udp6_sock',
 
             'struct __sk_buff',
             'struct sk_msg_md',
@@ -468,7 +467,6 @@ class PrinterHelpers(Printer):
             'struct tcp_sock',
             'struct tcp_timewait_sock',
             'struct tcp_request_sock',
-            'struct udp6_sock',
     }
     mapped_types = {
             'u8': '__u8',
