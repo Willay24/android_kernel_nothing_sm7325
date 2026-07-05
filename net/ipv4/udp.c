ac18793d25b9ec609c2ad0a16e6a75ad08040f75
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:43:23 +0600

    Revert "UPSTREAM: net: Track socket refcounts in skb_steal_sock()"
    This reverts commit f59a02f5cbcfe005134fd809c502804d7522354b.

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -2307,7 +2307,6 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	struct rtable *rt = skb_rtable(skb);
 	__be32 saddr, daddr;
 	struct net *net = dev_net(skb->dev);
-	bool refcounted;
 
 	/*
 	 *  Validate the packet.
@@ -2333,7 +2332,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	if (udp4_csum_init(skb, uh, proto))
 		goto csum_error;
 
-	sk = skb_steal_sock(skb, &refcounted);
+	sk = skb_steal_sock(skb);
 	if (sk) {
 		struct dst_entry *dst = skb_dst(skb);
 		int ret;
@@ -2342,8 +2341,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 			udp_sk_rx_dst_set(sk, dst);
 
 		ret = udp_unicast_rcv_skb(sk, skb, uh);
-		if (refcounted)
-			sock_put(sk);
+		sock_put(sk);
 		return ret;
 	}
 
