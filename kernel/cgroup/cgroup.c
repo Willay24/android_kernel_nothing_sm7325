bfb74f10d99318bc2576d0b0c2fa17c4e547617b
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:42:47 +0600

    Revert "UPSTREAM: cgroup: remove redundant kernfs_activate in cgroup_setup_root()"
    This reverts commit b7203cb42a703bdf7cd715520acd4b5715eeac84.

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2163,6 +2163,7 @@ int cgroup_setup_root(struct cgroup_root *root, u16 ss_mask)
 	BUG_ON(!list_empty(&root_cgrp->self.children));
 	BUG_ON(atomic_read(&root->nr_cgrps) != 1);
 
+	kernfs_activate(root_cgrp->kn);
 	ret = 0;
 	goto out;
 
