0ecd146345ce1cb7f1ccde7ad8c753e941ddf556
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:43:14 +0600

    Revert "UPSTREAM: bpf: Add bpf_skc_to_{tcp, tcp_timewait, tcp_request}_sock() helpers"
    This reverts commit 96a11e9fbaadcb6601d67636915071fce6a009b2.

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -423,9 +423,6 @@ class PrinterHelpers(Printer):
             'struct tcphdr',
             'struct seq_file',
             'struct tcp6_sock',
-            'struct tcp_sock',
-            'struct tcp_timewait_sock',
-            'struct tcp_request_sock',
 
             'struct __sk_buff',
             'struct sk_msg_md',
@@ -464,9 +461,6 @@ class PrinterHelpers(Printer):
             'struct tcphdr',
             'struct seq_file',
             'struct tcp6_sock',
-            'struct tcp_sock',
-            'struct tcp_timewait_sock',
-            'struct tcp_request_sock',
     }
     mapped_types = {
             'u8': '__u8',
