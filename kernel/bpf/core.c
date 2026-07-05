c9fbda371fa700eb0099c45b9ec12bdf83e7396a
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:42:52 +0600

    Revert "UPSTREAM: bpf: Fix potential race in tail call compatibility check"
    This reverts commit 1be0b79aa6400fda1b188d5c583233e60d476feb.

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1791,26 +1791,20 @@ static unsigned int __bpf_prog_ret0_warn(const void *ctx,
 bool bpf_prog_array_compatible(struct bpf_array *array,
 			       const struct bpf_prog *fp)
 {
-	bool ret;
-
 	if (fp->kprobe_override)
 		return false;
 
-	spin_lock(&array->aux->owner.lock);
-
-	if (!array->aux->owner.type) {
+	if (!array->aux->type) {
 		/* There's no owner yet where we could check for
 		 * compatibility.
 		 */
-		array->aux->owner.type  = fp->type;
-		array->aux->owner.jited = fp->jited;
-		ret = true;
-	} else {
-		ret = array->aux->owner.type  == fp->type &&
-		      array->aux->owner.jited == fp->jited;
+		array->aux->type  = fp->type;
+		array->aux->jited = fp->jited;
+		return true;
 	}
-	spin_unlock(&array->aux->owner.lock);
-	return ret;
+
+	return array->aux->type  == fp->type &&
+	       array->aux->jited == fp->jited;
 }
 
 static int bpf_check_tail_call(const struct bpf_prog *fp)
