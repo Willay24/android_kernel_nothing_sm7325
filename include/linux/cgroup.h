89cac45e21139b95638108dc8a21f48b38d7b5ef
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:43:35 +0600

    Revert "BACKPORT: kernfs: convert kernfs_node->id from union kernfs_node_id to u64"
    This reverts commit 626fc04169502832e392f27d81ff8f4eae35f960.

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -620,7 +620,7 @@ static inline bool cgroup_is_populated(struct cgroup *cgrp)
 /* returns ino associated with a cgroup */
 static inline ino_t cgroup_ino(struct cgroup *cgrp)
 {
-	return kernfs_ino(cgrp->kn);
+	return cgrp->kn->id.ino;
 }
 
 /* cft/css accessors for cftype->write() operation */
@@ -693,12 +693,13 @@ static inline void cgroup_kthread_ready(void)
 	current->no_cgroup_migration = 0;
 }
 
-static inline u64 cgroup_get_kernfs_id(struct cgroup *cgrp)
+static inline union kernfs_node_id *cgroup_get_kernfs_id(struct cgroup *cgrp)
 {
-	return cgrp->kn->id;
+	return &cgrp->kn->id;
 }
 
-void cgroup_path_from_kernfs_id(u64 id, char *buf, size_t buflen);
+void cgroup_path_from_kernfs_id(const union kernfs_node_id *id,
+					char *buf, size_t buflen);
 #else /* !CONFIG_CGROUPS */
 
 struct cgroup_subsys_state;
@@ -723,9 +724,9 @@ static inline int cgroup_init_early(void) { return 0; }
 static inline int cgroup_init(void) { return 0; }
 static inline void cgroup_init_kthreadd(void) {}
 static inline void cgroup_kthread_ready(void) {}
-static inline union u64 cgroup_get_kernfs_id(struct cgroup *cgrp)
+static inline union kernfs_node_id *cgroup_get_kernfs_id(struct cgroup *cgrp)
 {
-	return 0;
+	return NULL;
 }
 
 static inline struct cgroup *cgroup_parent(struct cgroup *cgrp)
@@ -749,8 +750,8 @@ static inline bool task_under_cgroup_hierarchy(struct task_struct *task,
 	return true;
 }
 
-static inline void cgroup_path_from_kernfs_id(u64 id, char *buf, size_t buflen)
-{}
+static inline void cgroup_path_from_kernfs_id(const union kernfs_node_id *id,
+	char *buf, size_t buflen) {}
 #endif /* !CONFIG_CGROUPS */
 
 #ifdef CONFIG_CGROUPS
