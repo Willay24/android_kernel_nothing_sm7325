a36df5df66e6a5b04ceb2be327ab2802dcabd26f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:43:09 +0600

    Revert "BACKPORT: udp_tunnel: add central NIC RX port offload infrastructure"
    This reverts commit 2b19312f622a9acbddd9a2295b879f1fe0edb9a8.

--- drivers/net/geneve.c
+++ drivers/net/geneve.c
@@ -1828,11 +1828,9 @@ static int geneve_netdevice_event(struct notifier_block *unused,
 	    event == NETDEV_UDP_TUNNEL_DROP_INFO) {
 		geneve_offload_rx_ports(dev, event == NETDEV_UDP_TUNNEL_PUSH_INFO);
 	} else if (event == NETDEV_UNREGISTER) {
-		if (!dev->udp_tunnel_nic_info)
-			geneve_offload_rx_ports(dev, false);
+		geneve_offload_rx_ports(dev, false);
 	} else if (event == NETDEV_REGISTER) {
-		if (!dev->udp_tunnel_nic_info)
-			geneve_offload_rx_ports(dev, true);
+		geneve_offload_rx_ports(dev, true);
 	}
 
 	return NOTIFY_DONE;
