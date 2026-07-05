f4c0517b8d1fe7c58a4c9764675e04d04b85486c
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-06 03:42:41 +0600

    Revert "UPSTREAM: bpfilter: switch bpfilter_ip_set_sockopt to sockptr_t"
    This reverts commit b644b034ebfe6a45a942922cb06cfde4f65620fc.

--- include/linux/bpfilter.h
+++ include/linux/bpfilter.h
@@ -4,10 +4,9 @@
 
 #include <uapi/linux/bpfilter.h>
 #include <linux/usermode_driver.h>
-#include <linux/sockptr.h>
 
 struct sock;
-int bpfilter_ip_set_sockopt(struct sock *sk, int optname, sockptr_t optval,
+int bpfilter_ip_set_sockopt(struct sock *sk, int optname, char __user *optval,
 			    unsigned int optlen);
 int bpfilter_ip_get_sockopt(struct sock *sk, int optname, char __user *optval,
 			    int __user *optlen);
@@ -17,7 +16,8 @@ struct bpfilter_umh_ops {
 	struct umd_info info;
 	/* since ip_getsockopt() can run in parallel, serialize access to umh */
 	struct mutex lock;
-	int (*sockopt)(struct sock *sk, int optname, sockptr_t optval,
+	int (*sockopt)(struct sock *sk, int optname,
+		       char __user *optval,
 		       unsigned int optlen, bool is_set);
 	int (*start)(void);
 };
