547768a71b27e5443a341510e63ae4d4cee9d519
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-07-08 04:20:22 +0600

    techpack: camera: sensor: Fix payload_count redefinition error
    Commit 4bc8ff3984e6 ("msm: camera: sensor: handling condition for random read")
    attempted to record the random read payload count in a local 'payload_count' variable inside
    cam_sensor_handle_random_read(). However, 'payload_count' is already passed to cam_sensor_handle_random_read() as an
    argument. This leads to the following compilation error:
    
    ../techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c:396:27: error: redefinition of 'payload_count' with a different type: 'int32_t' (aka 'int') vs 'uint32_t' (aka 'unsigned int')
      396 |         int32_t rc = 0, cnt = 0, payload_count = 0;
          |                                  ^
    ../techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c:393:42: note: previous definition is here
      393 |         struct cam_buf_io_cfg *io_cfg, uint32_t payload_count)
          |                                                 ^
    
    The only caller of cam_sensor_handle_random_read() is
    cam_sensor_i2c_command_parser() which parses 'payload_count' the same way locally.
    So, remove the duplicate local variable definition and its redundant assignment
    from cam_sensor_handle_random_read().
    
    Fixes: 4bc8ff3984e6 ("msm: camera: sensor: handling condition for random read")

--- techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -393,9 +393,8 @@ static int32_t cam_sensor_handle_random_read(
 	struct cam_buf_io_cfg *io_cfg, uint32_t payload_count)
 {
 	struct i2c_settings_list *i2c_list;
-	int32_t rc = 0, cnt = 0, payload_count = 0;
+	int32_t rc = 0, cnt = 0;
 
-	payload_count = cmd_i2c_random_rd->header.count;
 	i2c_list = cam_sensor_get_i2c_ptr(i2c_reg_settings,
 		payload_count);
 	if ((i2c_list == NULL) ||
