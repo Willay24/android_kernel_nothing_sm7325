750f80539a8f51bb9af053e0a245a6f8cbe6ebb6
William24hmar <williamhmar24@gmail.com>
2026-07-08 14:22:51 +0000

    Revert "BACKPORT: epoll: wire up syscall epoll_pwait2"
    This reverts commit b0f56c9401785077d75cb9cda299332159713e68.

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -854,11 +854,9 @@ __SYSCALL(__NR_clone3, sys_clone3)
 __SYSCALL(__NR_close_range, sys_close_range)
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
-#define __NR_epoll_pwait2 441
-__SC_COMP(__NR_epoll_pwait2, sys_epoll_pwait2, compat_sys_epoll_pwait2)
 
 #undef __NR_syscalls
-#define __NR_syscalls 442
+#define __NR_syscalls 441
 
 /*
  * 32 bit systems traditionally used different
