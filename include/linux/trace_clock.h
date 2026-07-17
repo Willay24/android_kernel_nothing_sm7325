a64f0d8345356702ebd0f73618e2c390610c376f
Rimaki <rimaki5891@gmail.com>
2026-07-17 23:48:18 +0700

    trace: Provide dummy trace_clock functions when TRACING is disabled
    When CONFIG_TRACING is disabled, the trace_clock functions become undefined. Provide dummy inline functions returning 0 to prevent build errors without cluttering other subsystems with #ifdefs.

--- include/linux/trace_clock.h
+++ include/linux/trace_clock.h
@@ -15,10 +15,18 @@
 
 #include <asm/trace_clock.h>
 
+#ifdef CONFIG_TRACING
 extern u64 notrace trace_clock_local(void);
 extern u64 notrace trace_clock(void);
 extern u64 notrace trace_clock_jiffies(void);
 extern u64 notrace trace_clock_global(void);
 extern u64 notrace trace_clock_counter(void);
+#else
+static inline u64 notrace trace_clock_local(void) { return 0; }
+static inline u64 notrace trace_clock(void) { return 0; }
+static inline u64 notrace trace_clock_jiffies(void) { return 0; }
+static inline u64 notrace trace_clock_global(void) { return 0; }
+static inline u64 notrace trace_clock_counter(void) { return 0; }
+#endif
 
 #endif /* _LINUX_TRACE_CLOCK_H */
