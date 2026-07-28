fa1c689f0f976b7108706a2242920c9e13152ec4
William24hmar <williamhmar24@gmail.com>
2026-07-28 20:57:25 +0000

    input: FocalTech: fix FB_EARLY_EVENT_BLANK error
     CC      drivers/input/touchscreen/usbtouchscreen.o
    ../drivers/input/touchscreen/focaltech_touch/focaltech_core.c:1563:20: error: use of undeclared identifier 'FB_EARLY_EVENT_BLANK'
     1563 |     if (!(event == FB_EARLY_EVENT_BLANK || event == FB_EVENT_BLANK))

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -38,7 +38,7 @@
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/power_supply.h>
-#if defined(CONFIG_FB)
+#if 0 && defined(CONFIG_FB)
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #elif defined(CONFIG_DRM)
@@ -1594,7 +1594,7 @@ static int fts_power_supply_callback(struct notifier_block *nb,
     return NOTIFY_OK;
 }
 
-#if defined(CONFIG_FB)
+#if 0 && defined(CONFIG_FB)
 static int fb_notifier_callback(struct notifier_block *self,
                                 unsigned long event, void *data)
 {
@@ -1937,7 +1937,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
     ts_data->pm_suspend = false;
 #endif
 
-#if defined(CONFIG_FB)
+#if 0 && defined(CONFIG_FB)
     ts_data->fb_notif.notifier_call = fb_notifier_callback;
     ret = fb_register_client(&ts_data->fb_notif);
     if (ret) {
@@ -2032,7 +2032,7 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
     if (ts_data->ts_workqueue)
         destroy_workqueue(ts_data->ts_workqueue);
 
-#if defined(CONFIG_FB)
+#if 0 && defined(CONFIG_FB)
     if (fb_unregister_client(&ts_data->fb_notif))
         FTS_ERROR("[FB]Error occurred while unregistering fb_notifier.");
 #elif defined(CONFIG_DRM)
