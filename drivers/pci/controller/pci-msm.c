dev-sm8350/kernel_oneplus_sm8350

treewide: Silence IPC log context creation failure messages
One can ask, why silence the IPC failure logs instead of guarding the entire
thing with CONFIG_IPC_LOGGING?

This is because scnprintf()/sprintf() formats a string and stores in a variable
that is used by ipc_log_context_create(). But ipc_log_context_create() is no-op
when CONFIG_IPC_LOGGING is disabled. The same applies to ipc_log_context_destroy()
as well. So, these pieces of code are never getting used and do not bring any
exceptional overhead.

Therefore, instead of guarding everything like a madman pointlessly, just silence
the IPC log context creation failure messages by demoting the error logs to debug.
There is no need to announce this failure so loudly and take over 5% of the dmesg.

Change-Id: b51a23cfc416e52ea3ffc89f16ef5865bb2ebcbb
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
Signed-off-by: TogoFire <togofire@mailfence.com>

drivers/pci/controller/pci-msm.c
@@ -7378,7 +7378,7 @@ static int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],

Willay24/android_kernel_nothing_sm7325

drivers/pci/controller/pci-msm.c
@@ -7378,7 +7378,7 @@ static int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			PCIE_DBG("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],