e3c1c36447ee1c8f0d32e95ef4e587089c75e1b2
William24hmar <williamhmar24@gmail.com>
2026-08-08 20:02:35 +0000

    Revert "syscall: Only spoof uname for root"
    This reverts commit 3ddda06098e8702dea1f068fe0060a14c1fd3e9d.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1270,11 +1270,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.10.239");
-			pr_debug("fake uname: %s/%d release=%s\n",
-				 current->comm, current->pid, tmp.release);
-		}
+		strcpy(tmp.release, "5.10.239");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
 
