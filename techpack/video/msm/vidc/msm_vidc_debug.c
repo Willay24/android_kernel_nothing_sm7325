08bebfc91698355de3cab1aec2b67b927dcc0d4a
William24hmar <williamhmar24@gmail.com>
2026-08-14 14:29:40 +0000

    Revert "techpack/video: msm: vidc: Disable debug logs"
    This reverts commit c9ee0ad46eb176ff2725764d8f4e51c6c0e2a91f.

--- techpack/video/msm/vidc/msm_vidc_debug.c
+++ techpack/video/msm/vidc/msm_vidc_debug.c
@@ -9,7 +9,8 @@
 #include "msm_vidc_debug.h"
 #include "vidc_hfi_api.h"
 
-int msm_vidc_debug = 0;
+int msm_vidc_debug = VIDC_ERR | VIDC_PRINTK |
+	FW_ERROR | FW_FATAL | FW_FTRACE;
 EXPORT_SYMBOL(msm_vidc_debug);
 
 bool msm_vidc_lossless_encode = !true;
