dcb28c7f8ae49673f7ce7fd8df24aa810f24613f
William24hmar <williamhmar24@gmail.com>
2026-08-14 17:11:26 +0000

    Revert "net-tcp_bbr: v2: record app-limited status of TLP-repaired flight"
    This reverts commit 53504f78e1f58452cab918c689a220809afdf8dd.

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -219,8 +219,7 @@ struct tcp_sock {
 	u16	advmss;		/* Advertised MSS			*/
 	u8	compressed_ack;
 	u8	tlp_retrans:1,	/* TLP is a retransmission */
-	tlp_orig_data_app_limited:1, /* app-limited before TLP rtx? */
-		unused_1:6;
+		unused_1:7;
 	u8	fast_ack_mode:2; /* which fast ack mode ? */
 	u32	chrono_start;	/* Start time in jiffies of a TCP chrono */
 	u32	chrono_stat[3];	/* Time in jiffies for chrono_stat stats */
