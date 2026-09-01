Thereache/scarlet_xiaomi_sm7325

techpack: touch: goodix: Destroy workqueues upon deinit
event_wq, gesture_wq and game_wq were never destroyed upon deinit.
Fix it.

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

techpack/touch/goodix_berlin_driver/goodix_ts_core.c
@@ -915,6 +915,12 @@ static void goodix_ts_sysfs_exit(struct goodix_ts_core *core_data)
 	sysfs_remove_group(&core_data->pdev->dev.kobj, &sysfs_group);
 }
 
+static void goodix_ts_wq_exit(struct goodix_ts_core *core_data)
+{
+	destroy_workqueue(core_data->gesture_wq);
+	destroy_workqueue(core_data->event_wq);
+}
+
 /* prosfs create */
 static int rawdata_proc_show(struct seq_file *m, void *v)
 {
@@ -2847,8 +2853,12 @@ static int goodix_ts_remove(struct platform_device *pdev)
 		goodix_ts_input_dev_remove(core_data);
 
 		goodix_fw_update_uninit();
+		goodix_ts_wq_exit(core_data);
 	}
 
+#ifdef GOODIX_XIAOMI_TOUCHFEATURE
+	destroy_workqueue(core_data->event_wq);
+#endif
 	goodix_tools_exit();
 	goodix_ts_unregister_notifier(&core_data->ts_notifier);
 	goodix_ts_power_off(core_data);

Willay24/android_kernel_nothing_sm7325

techpack/touch/goodix_berlin_driver/goodix_ts_core.c
@@ -910,6 +910,12 @@ static void goodix_ts_sysfs_exit(struct goodix_ts_core *core_data)
 	sysfs_remove_group(&core_data->pdev->dev.kobj, &sysfs_group);
 }
 
+static void goodix_ts_wq_exit(struct goodix_ts_core *core_data)
+{
+	destroy_workqueue(core_data->gesture_wq);
+	destroy_workqueue(core_data->event_wq);
+}
+
 /* prosfs create */
 static int rawdata_proc_show(struct seq_file *m, void *v)
 {
@@ -2845,8 +2851,12 @@ static int goodix_ts_remove(struct platform_device *pdev)
 		goodix_ts_input_dev_remove(core_data);
 
 		goodix_fw_update_uninit();
+		goodix_ts_wq_exit(core_data);
 	}
 
+#ifdef GOODIX_NOTHING_TOUCH
+	destroy_workqueue(core_data->event_wq);
+#endif
 	goodix_tools_exit();
 	goodix_ts_unregister_notifier(&core_data->ts_notifier);
 	goodix_ts_power_off(core_data);