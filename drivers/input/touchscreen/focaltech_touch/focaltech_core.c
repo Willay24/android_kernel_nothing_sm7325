c78289624dbe42b7ff244f61144e4961b9482357
William24hmar <williamhmar24@gmail.com>
2026-09-01 13:47:42 +0000

    Revert "input: FocalTech: fix FB_EARLY_EVENT_BLANK error"
    This reverts commit 969863e9279d752c6b4afffe3a7c4f28272cf6f3.

--- drivers/input/touchscreen/focaltech_touch/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_core.c
@@ -37,11 +37,10 @@
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
-#if 0 && defined(CONFIG_FB)
+#if defined(CONFIG_FB)
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #elif defined(CONFIG_DRM)
-#include <linux/notifier.h>
 #if defined(CONFIG_DRM_PANEL)
 #include <drm/drm_panel.h>
 #else
@@ -1547,7 +1546,7 @@ static void fts_resume_work(struct work_struct *work)
     fts_ts_resume(ts_data->dev);
 }
 
-#if 0 && defined(CONFIG_FB)
+#if defined(CONFIG_FB)
 static int fb_notifier_callback(struct notifier_block *self,
                                 unsigned long event, void *data)
 {
@@ -1889,7 +1888,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
     ts_data->pm_suspend = false;
 #endif
 
-#if 0 && defined(CONFIG_FB)
+#if defined(CONFIG_FB)
     ts_data->fb_notif.notifier_call = fb_notifier_callback;
     ret = fb_register_client(&ts_data->fb_notif);
     if (ret) {
@@ -1979,7 +1978,7 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
     if (ts_data->ts_workqueue)
         destroy_workqueue(ts_data->ts_workqueue);
 
-#if 0 && defined(CONFIG_FB)
+#if defined(CONFIG_FB)
     if (fb_unregister_client(&ts_data->fb_notif))
         FTS_ERROR("[FB]Error occurred while unregistering fb_notifier.");
 #elif defined(CONFIG_DRM)
