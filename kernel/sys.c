917323a1b672ffba814f5a3bac0f23a22ad24cb6
William24hmar <williamhmar24@gmail.com>
2026-09-15 21:37:39 +0000

    Import de-inlined hooks SUSFS v2.3.0
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1284,6 +1284,10 @@ static __always_inline bool should_spoof_uname(const char *comm)
 }
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern struct static_key_false susfs_is_uname_spoof_buffer_set;
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1292,6 +1296,10 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	if (static_branch_likely(&susfs_is_uname_spoof_buffer_set))
+		susfs_spoof_uname(&tmp);
+#endif
 #ifndef CONFIG_FAKE_UNAME_NONE
 	if (unlikely(should_spoof_uname(current->comm))) {
 		strscpy(tmp.release, FAKE_UNAME, sizeof(tmp.release));
