b9ce653d203d090e01cf628800d9b57ef644cd2a
William24hmar <williamhmar24@gmail.com>
2026-09-22 18:27:05 +0000

    techpack: Configure Nothing platform separate from lahaina
    from Nothing Phone(1) Nothing OS 3.0.A063(Spacewar-V3.0-250108-1938)

---
+++ techpack/audio/config/nothingautoconf.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ */
+
+#define CONFIG_NOTHING 1
+#define CONFIG_SND_SOC_TFA98XX 1
+#define CONFIG_TARGET_PRODUCT_SPACEWAR 1
